c725627914889b7f239df914adf47328b19eff09
MikhailSabanashvili <47075973+MikhailSabanashvili@users.noreply.github.com>
2021-04-01 21:53:31 +0300

    аутентификация

--- src/main/java/ru/sabanashvili/repositories/UsersRepositoryInMemoryImpl.java
+++ src/main/java/ru/sabanashvili/repositories/UsersRepositoryInMemoryImpl.java
@@ -1,33 +1,30 @@
 package ru.sabanashvili.repositories;
 
+import ru.sabanashvili.fake.FakeStorage;
 import ru.sabanashvili.modules.User;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 //класс-хранилище, который возвращает трех пользаков
 
 public class UsersRepositoryInMemoryImpl implements UsersRepository {
-    private List<User> users;
-
-    public UsersRepositoryInMemoryImpl() {
-        this.users = new ArrayList<>();
-        User user = new User("Misha", "qwerty", LocalDate.parse("1994-05-13"));
-        User user1 = new User("Misha1", "qwerty", LocalDate.parse("1994-05-13"));
-        User user2 = new User("Misha2", "qwerty", LocalDate.parse("1994-05-13"));
-        users.add(user);
-        users.add(user1);
-        users.add(user2);
-    }
-
     public List<User> findAll(){
-        return this.users;
+        return FakeStorage.storage().users();
     }
 
     @Override
     public void save(User user) {
-        users.add(user);
+        FakeStorage.storage().users().add(user);
+    }
+
+    @Override
+    public boolean isExist(String name, String password) {
+        for (User user: FakeStorage.storage().users()) {
+            if(user.getName().equals(name) &&
+            user.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
